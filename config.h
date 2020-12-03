3780bbe123a977583e8f7fc98f7eccd1c1f3af89
Michael Y. C. Shih <i420cc@gmail.com>
2020-12-03 15:27:37 +0800

    auto push

--- config.h
+++ config.h
@@ -248,7 +248,8 @@ static Key keys[] = {
 	{ MODKEY,			XK_F10,		spawn,		SHCMD("dmenuumount") },
 	{ MODKEY,			XK_F11,		spawn,		SHCMD("mpv --no-cache --no-osc --no-input-default-bindings --input-conf=/dev/null --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1)") },
 	/* { MODKEY,			XK_F12,		xrdb,		{.v = NULL } }, */
-	{ MODKEY,			XK_space,	zoom,		{0} },
+	/* { MODKEY,			XK_space,	zoom,		{0} }, */
+	{ MODKEY,			XK_space, spawn,    SHCMD("rofi -show drun") },
 	{ MODKEY|ShiftMask,		XK_space,	togglefloating,	{0} },
 
 	{ 0,				XK_Print,	spawn,		SHCMD("maim pic-full-$(date '+%y%m%d-%H%M-%S').png") },
